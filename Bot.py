23db3db28949663d3cbb67115f1cb20bbec64336
vladeeer <53297454+vladeeer@users.noreply.github.com>
2021-12-25 18:17:26 +0300

    Add files via upload

--- Bot.py
+++ Bot.py
@@ -7,13 +7,56 @@ import Func as F
 
 print("Bot strarted...")
 
+def typing(update, n):
+    for i in range(0, n):
+        dp.bot.send_chat_action(chat_id=update.message.chat_id, action="typing")
+        time.sleep(1)
+
 def log_message(name, text):
-    dp.bot.send_message(chat_id=-530324262, text=(name + ": " + text + "\n"))
+    for log_chat in keys.LOG_CHATS:
+        dp.bot.send_message(chat_id=log_chat, text=(name + ": " + text + "\n"))
 
 def start_command(update, context):
     log_message(update.message.from_user.first_name + " " + ("" if update.message.from_user.last_name is None else str(update.message.from_user.last_name)) + " [" + str(update.message.from_user.id) + "]: ", "Joined chat with Abdula")
-    log_message("Bot", "Ээйа")
-    update.message.reply_text("Ээйа")
+    if (("лег" in update.message.from_user.first_name) or ("лад" in update.message.from_user.first_name)):
+        log_message("Bot", "С нг крч")      
+        time.sleep(6)
+        typing(update, 4)
+        update.message.reply_text("Э ну привет")
+        time.sleep(2)
+        typing(update, 1)
+        time.sleep(7)
+        typing(update, 20)
+        update.message.reply_text("Вопщим сидел я аднажды в талете и думал о сваиом будущим но какда настала время вытерать нис я пасматрел на абрывак сральнай ленты у миня в руке")
+        time.sleep(2)
+        typing(update, 10)
+        update.message.reply_text("Я понял што этат абрывак был неабычный он был асобеный")
+        time.sleep(3)
+        typing(update, 1)
+        time.sleep(10)
+        typing(update, 10)
+        update.message.reply_text("Я вдрук пачуствавал што имена этат абрывак должын стать падаркам тибе на новий год")
+        time.sleep(2)
+        typing(update, 12)
+        update.message.reply_text("На абрыфке я написал сваио имя и атправил ево к тибе")
+        time.sleep(5)
+        typing(update, 25)
+        update.message.reply_text("И вот типерь када ты написал мине я хачу пасдравит тибя с самым пркирасным празником в гаду новым годам!!")
+        time.sleep(3)
+        typing(update, 9)
+        update.message.reply_text("Пуст новый гот станит лутше чем паследний")
+        time.sleep(1)
+        typing(update, 4)
+        update.message.reply_text("Щасливее висилее ярчи")
+        time.sleep(2)
+        typing(update, 13)
+        update.message.reply_text("Здаровия тибе дастатка улыбак удачи и самих лутшых дыней!")
+        time.sleep(1)
+        typing(update, 5)
+        update.message.reply_text("С новым годам!!!")
+    else:
+        log_message("Bot", "Ээйа")
+        update.message.reply_text("Ээйа")
 
 def help_command(update, context):
     log_message(update.message.from_user.first_name, "/help")
@@ -24,8 +67,9 @@ def handle_message(update, context):
     if (update.message != None):
         text=str(update.message.text).lower()
         log_message(update.message.from_user.first_name, update.message.text)
-        if ((update.message.from_user.id == int(keys.ADMIN)) and (text[0]=="/")):
-            print("admin")
+        if ((update.message.from_user.id in keys.ADMINS) and (text[0]=="/")):
+            print("Admin: " + text)
+            log_message("[Admin]",text)
         else:
             resp = F.message_responses(text, update.message.from_user.first_name)
             for i in resp:
--- Func.py
+++ Func.py
@@ -13,40 +13,45 @@ def message_responses(input_text, user_name):
         resp.append(random.choice(["Ээ брат","Нетрогаи Влада","Ща пизды палучиш"]))
         resp.append(random.choice(["За Влада и дывор стреляйу вупор","Паясьни за Влада","Я за ниго гарой"]))
         
-    elif(lookFor(["леш","лёх","лёш"], user_message)):
-        resp.append(random.choice(["Я нэ понил","ээээ"]))
-        resp.append(random.choice(["за лёху тоби пизида","ево не трогой ато наиду"]))
+    elif(lookFor(["олег","олеж"], user_message)):
+        resp.append(random.choice(["Я нэ понил","Ээээ"]))
+        resp.append(random.choice(["За Алежу тоби пизида","Ево не трогой ато наиду","За Алежу и дывор стреляйу вупор"]))
+
+    elif(lookFor(["пасиб","спс","благод"], user_message)):
+        resp.append(random.choice(["Незашта","Нестоет"]))
+        resp.append(random.choice(["Рат был стараца","Всигда пажаласта"]))
 
     elif(lookFor(["иди ","пош","нах"], user_message)):
-        resp.append(random.choice(["куда","эээ сам иди","а ты неахуел"]))
+        resp.append(random.choice(["Куда","Эээ сам иди","А ты неахуел"]))
         resp.append(random.choice(["К маме тваей пайду",""]))
 
-    elif(lookFor(["лох","пид","придур","баран","идио","сук","дурак","мраз","дубин","уёб","уебан","даун","ебанут","оёб"], user_message)):
+    elif(lookFor(["лох","пид","придур","баран","идио","сук","дурак","мраз","дубин","уёб","уебан","даун","ебанут","оёб","урод","суч"], user_message)):
         resp.append(random.choice(["Ээээ","Слыш мудила","Ты че баран","Слыш агрызак капусты","Ты че"]))
         resp.append(random.choice(["Я тибя найду", "А ты ни ахуэл","Овца тупая","Я тибя вычеслю уебак","ديك مص"]))
         resp.append(random.choice(["Атветиш за слава","Рот патом не актроеж","Капсда тебе","",""]))
-
+    
     elif(lookFor(["прив","добр","здра","сал","йо","даро","хай","ку"], user_message)):
-        resp.append(random.choice(["И еcчо рас здравстуйти", "Да здастуйте", "Салам","Привет","Салам алейкум"]))
-        resp.append(random.choice(["Са мной редка сдароваюса","","","","","",""]))
+        resp.append(random.choice(["И еcчо рас здравстуйти", "Да здастуйте", "Салам","Привет","Салам алейкум","Ассаля́му але́йкум","اَلسَّلَامُ عَلَيْكُمُ‏"]))
+        resp.append(random.choice(["Са мной ретка сдароваюса","","","","","","",""]))
 
     elif(lookFor(["че","чо","ээ","непон","чё"], user_message)):
-        resp.append(random.choice(["ты чо борзый","слыш ты че","харэ дрожнить","Ээ",""]))
+        resp.append(random.choice(["Ты чо борзый","Слыш ты че","Харэ дрожнить","Ээ",""]))
         resp.append(random.choice(["Я не всекда такоц добрый","Атхватить так можеж","",""]))
 
     elif (lookFor(["зовут","звать","назыв","имя","кто"], user_message)):
         resp.append(random.choice(["Я абдула","Абдула мама звола",""]))
         resp.append(random.choice(["Друзья завут Дюдя как член каня",""]))
+
+    elif (lookFor(["врем","час"], user_message)):
+        now = datetime.now()
+        date_time = now.strftime("%H %M")
+        date_time2 = now.strftime("%d/%m/%y") 
+
+        resp.append(random.choice(["Сичас ","У миня на чисах ","Друк сказыал "])+str(date_time))
         
     elif("?" in user_message):
         resp.append(random.choice(["Да","Нет","Наверна","Я не знаьу","Ага","Нее","Канешна"])) 
         resp.append(random.choice(["А ты","хатя может быть","А сам","","","",""])) 
-
-    elif user_message in ("time", "time?"):
-        now = datetime.now()
-        date_time = now.strftime("%d/%m/%y, %H:%M:%S")
-
-        resp.append(str(date_time))
 
     if (len(resp)==0):
         resp = [random.choice(["Непонел","Чо","Гавари нармальна","Эээээ","Я плоха панимаю","Эээ","Говари нармальна пажалуста","Я не панимаю","Че","А","Паруски скажы"])]
